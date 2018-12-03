0cc0f0ab82614838a29befcadb8600b20d31325e
Maia <28684401+Maia313@users.noreply.github.com>
2018-12-03 16:06:45 +0100

    Update react-hooks.js

--- react-hooks.js
+++ react-hooks.js
@@ -1,1 +1,20 @@
+#### Hooks
+* `useState`:
+  ```js
+  import React, {useState} from 'react'
 
+  function Counter() {
+    const [count, setCount] = useState(0)
+    const increment = () => setCount(count + 1)
+    return <button onClick={increment}>{count}</button>
+  }
+
+  export default Counter
+```
+* `useReducer`:
+
+
+* `useContext`:
+
+
+* `useEffect`:
